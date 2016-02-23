d0651ccfe6e996415a88e6bbb7df7fe927f81bc3
Stefan Oravec <stefan.oravec@me.com>
2016-02-23 14:03:44 +0100

    tradaa

--- sqlsnrc.sql
+++ sqlsnrc.sql
@@ -4,3 +4,5 @@ define g_sqlsn_modules_path = 'oradb_modules/sqlsn/sqlsn_modules'
 --init sqlsn-core module
 set verify off
 @&&g_sqlsn_modules_path/sqlsn-core/module.sql "&&g_sqlsn_modules_path/sqlsn-core" 
+
+prompt TRADAA
